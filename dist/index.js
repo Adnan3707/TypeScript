5b46d22360c79136d597111b731c75ccf5496d98
unknown <adnanfarooq37@gmail.com>
2025-01-19 18:55:07 +0530

    initial

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let number = 2;
+let course = 'TypeScript';
+let isPublished = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+// render(2);
+let numbers = [1, 2, 3, 4, 5];
+// casting
+function casting() {
+    let age = 20;
+    age = 21;
+    console.log(age);
+}
+// casting()
+//return type
+function add(a, b) {
+    return a + b;
+}
+console.log(add('2', 2));
+function retArr() {
+    return [1, 2, 3, 4, 5];
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+var number = 2;
+var course = 'TypeScript';
+var isPublished = true;
+var level;
+function render(document) {
+    console.log(document);
+}
+// render(2);
+var numbers = [1, 2, 3, 4, 5];
+// casting
+function casting() {
+    var age = 20;
+    console.log(age);
+}
+casting();
